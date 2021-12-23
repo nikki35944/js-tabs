fd762c32c070656ab894726bee72edbf679c6775
nikki35944 <95740936+nikki35944@users.noreply.github.com>
2021-12-23 18:16:48 +0300

    added
    js tabs

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+    //Объявляем переменные селекторы для пунктов меню и контента табов
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    //скрываем контент табы на странице
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    //Функция для удуления css класса hide и добавления show
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    // При клике на кнопки меню убираем контент который показывается
+    // по умолчанию и выводим тот, по которому кликнули
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+});
